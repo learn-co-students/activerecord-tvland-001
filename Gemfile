b5dc5e136ae33b935b6d0385f7a869fcc82ac0eb
Chris Aporta <christopher.aporta@gmail.com>
2015-12-08 20:48:42 -0500

    complete lab

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source "https://rubygems.org"
 gem 'pry'
 gem "rspec"
+gem "learn-co"
 gem "activerecord"
 gem "sqlite3"
 gem "rake"
--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,16 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, :through => :characters
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def list_roles
+    Character.where(:actor_id => id).each do |character|
+      role = character.name
+      show = Show.find_by(:id => character.show_id).name
+      return "#{role} - #{show}"
+    end
+  end
 end
