77eacbbdc626f35e13d170b17c9782908593f4ce
maiya777 <maiya777@list.ru>
2015-12-17 22:59:39 +0300

    Done

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,12 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{self.first_name.capitalize} #{self.last_name.capitalize}"
+  end 
+
+  def list_roles
+    self.characters.map {|ch| "#{ch.name} - #{ch.show.name}"}.join(", ")
+  end
 end
