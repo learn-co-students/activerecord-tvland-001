df21d358516668ad873f98cff91a79bdc95230ab
epang1 <epang1@gmail.com>
2016-12-04 20:17:40 -0500

    done

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,20 @@
+require 'pry'
+
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    all_roles = []
+    self.characters.each do |character|
+      all_roles << "#{character.name} - #{Show.find(character.show_id).name}"
+    end
+
+    all_roles
+  end
+
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,14 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :actor
+  belongs_to :show
+
+  def say_that_thing_you_say
+    return "#{self.name} always says: #{self.catchphrase}"
+  end
+
+  def build_show(info)
+    new_show = Show.new(info)
+    self.show = new_show
+  end
+
 end
