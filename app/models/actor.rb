01c4855fa1f50052140a26aa3f2affbc8d7b30d1
halfields <hfields697@aol.com>
2017-08-12 00:22:10 -0500

    migrations

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,17 @@
-class Actor < ActiveRecord::Base
+class Actor < ActiveRecord::Base 
+
+	has_many :characters
+	has_many :shows, through: :characters
+	has_many :networks, through: :shows
+
+	def full_name
+		first = self.first_name
+		last = self.last_name
+		"#{first} #{last}"
+	end
+
+	def list_roles
+		self.characters
+	end
   
 end
