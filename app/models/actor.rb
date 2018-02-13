a123a6182e725e64277d10cf917cd75309b474e6
andrewkjankowski <andrewkjankowski@gmail.com>
2018-02-13 15:33:57 -0500

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,17 @@
+require 'pry'
+
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    roles = []
+    self.characters.each do |c|
+      roles << "#{c.name} - #{c.show.name}"
+    end
+    roles
+  end
 end
---
+++ db/migrate/20180213203232_add_data_to_shows.rb
@@ -0,0 +1,9 @@
+class AddDataToShows < ActiveRecord::Migration
+  def change
+    change_table :shows do |t|
+      t.string :day
+      t.string :season
+      t.string :genre
+    end
+  end
+end
